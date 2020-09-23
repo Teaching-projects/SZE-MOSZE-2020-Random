87962a9ccac42948e57c21c6e4f3503685e14c52
LengyHELL <ubi19998@gmail.com>
2020-09-23 18:37:29 +0200

    Implementing parser function and removing the attack writeout

--- unit.cpp
+++ unit.cpp
@@ -1,15 +1,86 @@
 #include "unit.hpp"
+#include <fstream>
 
 std::ostream& operator<<(std::ostream& os, const Unit& unit) {
   os << unit.get_name() << "(HP:" << unit.get_health() << ", DMG:" << unit.get_damage() << ")";
   return os;
 }
 
+void Unit::parse_unit(const std::string& filename) {
+  std::ifstream file(filename);
+  if (file.is_open()) {
+    char c;
+    std::string sep = "{,}";
+    std::string data = "";
+    while(file.get(c)) {
+      if (!isspace(c)) { data += c; }
+    }
+
+    if (data.front() != '{') { throw UnitException("Invalid syntax!"); }
+
+    int begin = data.find_first_of(sep) + 1;
+    int end = 0;
+    bool close = false;
+    while (end < (int)data.size() - 1) {
+      if (close) { throw UnitException("Invalid syntax!"); }
+
+      end = data.find_first_of(sep, begin);
+
+      if (end == -1) { throw UnitException("Invalid syntax!"); }
+
+      std::string line = data.substr(begin, end - begin);
+
+      if (data[end] == '}') { close = true; }
+      else { begin = end + 1; }
+
+      //processing line
+      int count = 0;
+      for (unsigned i = 0; i < line.size(); ++i) {
+        if (line[i] == ':') { count++; }
+      }
+      if (count != 1) { throw UnitException("Invalid syntax!"); }
+
+      std::string tag = line.substr(0, line.find(":"));
+      std::string value = line.substr(line.find(":") + 1);
+
+      if ((tag.front() != '\"') || (tag.back() != '\"')) { throw UnitException("Invalid syntax!"); }
+
+      tag = tag.substr(1, tag.size() - 2);
+
+      if (tag.size() <= 0) { throw UnitException("Invalid syntax!"); }
+
+      if (value.size() <= 0) { throw UnitException("Invalid syntax!"); }
+
+      //check tags
+      if (tag == "name") {
+        if ((value.front() != '\"') || (value.back() != '\"')) { throw UnitException("Invalid syntax!"); }
+        value = value.substr(1, value.size() - 2);
+
+        if (value.size() <= 0) { throw UnitException("Invalid syntax!"); }
+
+        name = value;
+      }
+      else if (tag == "hp") {
+        health = std::stoi(value);
+      }
+      else if (tag == "dmg") {
+        damage = std::stoi(value);
+      }
+    }
+
+    file.close();
+  }
+  else {
+    //throw exception
+    throw UnitException("Failed to open file!");
+  }
+}
+
 void Unit::attack(Unit& other) const {
   if (health > 0) {
-    std::cout << std::endl << name << " attacks " << other.get_name() << std::endl;
+    //std::cout << std::endl << name << " attacks " << other.get_name() << std::endl;
     other.suffer_damage(damage);
-    std::cout << *this << std::endl << other << std::endl;
+    //std::cout << *this << std::endl << other << std::endl;
   }
 }
 

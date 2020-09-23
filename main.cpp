78f0f3151c6d976a52834ef6cfc1e2652a8f350c
LengyHELL <ubi19998@gmail.com>
2020-09-23 18:35:40 +0200

    Adapting to new functions and slightly changing the output

--- main.cpp
+++ main.cpp
@@ -3,25 +3,16 @@
 
 int main(int argc, char** argv) {
 
-  if (argc != 7) {
-    std::cerr << "Invalid arguments!" << std::endl;
+  Unit m1, m2;
+
+  try {
+    m1.parse_unit(argv[1]);
+    m2.parse_unit(argv[2]);
+  }
+  catch(const UnitException& e) {
+    std::cerr << e.what() << std::endl;
     return 1;
   }
-
-  float hp1 = (float)strtod(argv[2], NULL);
-  float hp2 = (float)strtod(argv[5], NULL);
-  float dmg1 = (float)strtod(argv[3], NULL);
-  float dmg2 = (float)strtod(argv[6], NULL);
-
-  if ((hp1 <= 0) || (hp2 <= 0) || (dmg1 <= 0) || (dmg2 <= 0)) {
-    std::cerr << "Invalid arguments!" << std::endl;
-    return 1;
-  }
-
-  Unit m1(argv[1], hp1, dmg1);
-  Unit m2(argv[4], hp2, dmg2);
-
-  std::cout << m1 << " vs. " << m2 << std::endl;
 
   while ((m1.get_health() > 0) && (m2.get_health() > 0)) {
     m1.attack(m2);
@@ -29,10 +20,10 @@ int main(int argc, char** argv) {
   }
 
   if (m1.get_health() <= 0) {
-    std::cout << std::endl << m2 << " wins!" << std::endl;
+    std::cout << m2.get_name() << " wins! Remaining HP:" << m2.get_health() << std::endl;
   }
   else {
-    std::cout << std::endl << m1 << " wins!" << std::endl;
+    std::cout << m1.get_name() << " wins! Remaining HP: " << m1.get_health() << std::endl;
   }
 
   return 0;

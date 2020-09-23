3ed6fd238738d78257a347891f236594ec6359be
LengyHELL <ubi19998@gmail.com>
2020-09-23 22:20:36 +0200

    Adding parameters

--- attack_test.sh
+++ attack_test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-./a.out units/ka.json units/ke.json > attack_test.txt
-./a.out units/ka.json units/ki.json >> attack_test.txt
-./a.out units/ke.json units/ki.json >> attack_test.txt
+./a.out units/ka.json units/ke.json > $1
+./a.out units/ka.json units/ki.json >> $1
+./a.out units/ke.json units/ki.json >> $1
 
-./a.out units/ke.json units/ka.json >> attack_test.txt
-./a.out units/ki.json units/ka.json >> attack_test.txt
-./a.out units/ki.json units/ke.json >> attack_test.txt
+./a.out units/ke.json units/ka.json >> $1
+./a.out units/ki.json units/ka.json >> $1
+./a.out units/ki.json units/ke.json >> $1
 
-diff attack_test.txt attack_test_target.txt
+diff $1 $2
+
+exit $?

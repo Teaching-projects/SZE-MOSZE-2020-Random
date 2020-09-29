c5a8ee0753a2cc66b721b0000be4789457106a06
LengyHELL <ubi19998@gmail.com>
2020-09-29 09:46:22 +0200

    Adapting to new functions and adding parameter check

--- main.cpp
+++ main.cpp
@@ -5,13 +5,18 @@ int main(int argc, char** argv) {
 
   Unit m1, m2;
 
+  if (argc != 3) {
+    std::cerr << "Application needs 2 parameters!" << std::endl;
+    return 1;
+  }
+
   try {
-    m1.parse_unit(argv[1]);
-    m2.parse_unit(argv[2]);
+    m1 = Unit::parse_unit(argv[1]);
+    m2 = Unit::parse_unit(argv[2]);
   }
   catch(const UnitException& e) {
     std::cerr << e.what() << std::endl;
-    return 1;
+    return 2;
   }
 
   while ((m1.get_health() > 0) && (m2.get_health() > 0)) {

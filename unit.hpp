b46e02a8111900e0929f8cbbe5fd3d5ab716a25e
LengyHELL <ubi19998@gmail.com>
2020-10-14 13:58:00 +0200

    Adding cooldown variables and elapse_time function, modifying constructors and attack function

--- unit.hpp
+++ unit.hpp
@@ -16,10 +16,14 @@ class Unit {
   std::string name = "";
   float health = 0;
   float damage = 0;
+  float max_cd = 0;
+  float cd = 0;
+
+  void suffer_damage(const float& damage);
 
 public:
-  Unit() : name(""), health(0), damage(0) {}
-  Unit(const std::string& n, const float& h, const float& d) : name(n), health(h), damage(d) {}
+  Unit() : name(""), health(0), damage(0), max_cd(0), cd(0) {}
+  Unit(const std::string& n, const float& h, const float& d, const float& cd) : name(n), health(h), damage(d), max_cd(cd), cd(0) {}
   //~Unit() {}
 
   //operator overload
@@ -27,8 +31,8 @@ public:
 
   //functions
   static Unit parse_unit(const std::string& filename);
-  void attack(Unit& other) const;
-  void suffer_damage(const float& damage);
+  void attack(Unit& other);
+  void elapse_time(const float& t);
 
   //getters
   std::string get_name() const { return name; }

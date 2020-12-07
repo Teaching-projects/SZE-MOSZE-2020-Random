2b0ff4d8df530c877c4769fbddb821328ce6c239
LengyHELL <ubi19998@gmail.com>
2020-12-07 11:30:37 +0100

    Fixing error handling

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,13 +7,22 @@
 #include <list>
 
 #include "JSON.h"
+#include "Map.h"
+#include "Game.h"
 #include "PreparedGame.h"
 
 const std::map<int,std::string> error_messages = {
   { 1 , "Bad number of arguments. Only a single configuration file should be provided." },
   { 2 , "The provided configuration file is not accessible." },
-  { 3 , "The provided scenario file is invalid." },
-  { 4 , "JSON parsing error." }
+  { 3 , "JSON parsing error." },
+  { 4 , "The provided map file is not accessible." },
+  { 5 , "The provided map file is invalid." },
+  { 6 , "Invalid access on map." },
+  { 7 , "Multiple hero placement is forbidden." },
+  { 8 , "Map change while units are initialized is forbidden." },
+  { 9 , "Tried to change the game after it has been started." },
+  { 10 , "Game not initialized properly." },
+  { 11 , "The given position is not accessible." }
 };
 
 void bad_exit(int exitcode){
@@ -32,8 +41,31 @@ int main(int argc, char** argv){
   }
   catch (const JSON::ParseException& e) {
     std::cerr << e.what() << std::endl;
+    bad_exit(3);
+  }
+  catch ( const Map::FileNotFoundException& e) {
     bad_exit(4);
   }
-
+  catch ( const Map::InvalidFileException& e) {
+    bad_exit(5);
+  }
+  catch ( const Map::WrongIndexException& e) {
+    bad_exit(6);
+  }
+  catch ( const Game::AlreadyHasHeroException& e) {
+    bad_exit(7);
+  }
+  catch ( const Game::AlreadyHasUnitsException& e) {
+    bad_exit(8);
+  }
+  catch ( const Game::GameAlreadyStartedException& e) {
+    bad_exit(9);
+  }
+  catch ( const Game::NotInitializedException& e) {
+    bad_exit(10);
+  }
+  catch ( const Game::OccupiedException& e) {
+    bad_exit(11);
+  }
   return 0;
 }
